878001ab42349e2d3496799f0204173cc4a638af
Toby Clemson <tobyclemson@gmail.com>
2017-12-03 14:01:14 +0000

    Pseudocode out lambda.

--- lambdas/ecs_route53_registration/ecs_route53_registration_lambda.py
+++ lambdas/ecs_route53_registration/ecs_route53_registration_lambda.py
@@ -19,3 +19,30 @@ def amend_route53_for(event, context):
         'Event is for container instance with ARN: %s in cluster with ARN: %s',
         cloud_watch_ecs_event.container_instance_arn(),
         cloud_watch_ecs_event.cluster_arn())
+
+    # if event corresponds to the service we are managing
+    #   if event indicates a newly running task or
+    #      event indicates a newly stopped task
+    #     determine running tasks
+    #     determine DNS name for each task
+    #       fetch instance details
+    #       populate DNS template
+    #     add/update record in Route53
+    # else
+    #   ignore
+
+    # types:
+    #   CloudWatchECSEvent
+    #     - tells us things about the received event
+    #     - e.g., container instance ARN, cluster ARN, service name
+    #     - actually may only need state change and service name since
+    #       looking up the tasks should give container instance details
+    #   ECSService
+    #     - has many running ECSTasks
+    #   ECSContainerInstance
+    #     - tells us things about the container instance like IP address
+    #   ECSTask
+    #     - has many ECSContainerDefinitions
+    #   ECSContainerDefinition
+    #     - knows about what port the container listens on
+    #
