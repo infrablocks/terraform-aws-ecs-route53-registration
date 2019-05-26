22de41a8deb63eb7a467090494658b6bdebaaf1b
Toby Clemson <tobyclemson@gmail.com>
2019-05-27 00:10:54 +0100

    Install more libs for python.

--- scripts/ci/common/install-pyenv.sh
+++ scripts/ci/common/install-pyenv.sh
@@ -5,7 +5,10 @@ set -e
 set -o pipefail
 
 apt-get update
-apt-get install -y --no-install-recommends build-essential curl
+apt-get install -y --no-install-recommends \
+    make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
+    libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils \
+    tk-dev libffi-dev liblzma-dev python-openssl git
 
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
